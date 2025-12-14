d79554fcf22516bf28351aafc1b29a24cb25f0e7
ShammiT06 <shammikumar.fullstack@gmail.com>
2025-12-14 16:53:58 +0530

    age screen updated

--- app/(screens)/index.jsx
+++ app/(screens)/index.jsx
@@ -1,13 +1,63 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { FontAwesome } from "@expo/vector-icons";
+import { useState } from "react";
+import Styles from "../../assets/Styles/screens-styles/age.styles";
 
-export default function index()
-{
+export default function Index() {
+    const [selectedGender, setSelectedGender] = useState(null);
+    console.log(selectedGender)
+
     return (
-    <SafeAreaView>
-        <View>
-            <Text>Hello</Text>
-        </View>
-    </SafeAreaView>
-)
+        <SafeAreaView style={Styles.view}>
+            <View style={Styles.container}>
+                <View style={Styles.headerCont}>
+                    <Text style={Styles.Header}>What’s Your Gender</Text>
+                    <Text style={Styles.subText}>
+                        Select your gender to personalize your experience
+                    </Text>
+                </View>
+                <View style={Styles.genderContainer}>
+                    <TouchableOpacity
+                        style={[
+                            Styles.circle,
+                            selectedGender === "male"
+                                ? Styles.activeCircle
+                                : Styles.inactiveCircle,
+                        ]}
+                        onPress={() => setSelectedGender("male")}
+                    >
+                        <FontAwesome
+                            name="mars"
+                            size={50}
+                            color={selectedGender === "male" ? "#fff" : "#222"}
+                        />
+                    </TouchableOpacity>
+                    <Text style={Styles.genderText}>Male</Text>
+
+                    <TouchableOpacity
+                        style={[
+                            Styles.circle,
+                            selectedGender === "female"
+                                ? Styles.activeCircle
+                                : Styles.inactiveCircle,
+                        ]}
+                        onPress={() => setSelectedGender("female")}
+                    >
+                        <FontAwesome
+                            name="venus"
+                            size={50}
+                            color={selectedGender === "female" ? "#fff" : "#222"}
+                        />
+                    </TouchableOpacity>
+                    <Text style={Styles.genderText}>Female</Text>
+                </View>
+                <View style={Styles.confmBtn}>
+                    <TouchableOpacity>
+                        <Text style={Styles.btnTxt}>Continue</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </SafeAreaView>
+    );
 }
---
+++ assets/Styles/screens-styles/age.styles.js
@@ -0,0 +1,80 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+const { width } = Dimensions.get("window");
+const CIRCLE_SIZE = width * 0.45;
+
+const Styles = StyleSheet.create({
+    view: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+
+    container: {
+        flex: 1,
+        paddingHorizontal: width * 0.06,
+    },
+
+    headerCont: {
+        marginTop: width * 0.1,
+        marginBottom: width * 0.12,
+    },
+
+    Header: {
+        fontSize: width * 0.065,
+        fontWeight: "700",
+        textAlign: "center",
+    },
+
+    subText: {
+        marginTop: 10,
+        textAlign: "center",
+        fontSize: width * 0.038,
+        color: "#666",
+        lineHeight: 22,
+    },
+
+    genderContainer: {
+        alignItems: "center",
+    },
+
+    circle: {
+        width: CIRCLE_SIZE,
+        height: CIRCLE_SIZE,
+        borderRadius: CIRCLE_SIZE / 2,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: width * 0.08,
+    },
+
+    activeCircle: {
+        backgroundColor: "#59C7CB",
+    },
+
+    inactiveCircle: {
+        backgroundColor: "#fff",
+        borderWidth: 1.5,
+        borderColor: "#59C7CB",
+    },
+
+    genderText: {
+        marginTop: 12,
+        fontSize: width * 0.045,
+        fontWeight: "600",
+    },
+    confmBtn: {
+        backgroundColor: "#59C7CB",
+        width: width * 0.70,
+        justifyContent: "center",
+        alignItems: "center",
+        alignSelf: "center",
+        height: width * 0.158,
+        marginTop:width * 0.15,
+        borderRadius:15,
+    },
+    btnTxt:{
+        color:"#fff",
+        fontSize:width * 0.065,
+    }
+});
+
+export default Styles;
